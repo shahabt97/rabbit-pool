5e60d86c5ea5520c95364686b258f0e2c4e63a2e
shahabt97 <shahab.ta97@gmail.com>
2024-06-24 08:47:11 +0330

    refact consumer

--- consumer.go
+++ consumer.go
@@ -1,42 +1,37 @@
 package rabbitmq
 
 import (
-	"errors"
-
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type Consumer struct {
-	Pool  *ConnectionPool
-	Queue string
+	pool         *ConnectionPool
+	queueName    string
+	durable      bool
+	exchangeName string
+	routKey      string
 }
 
-func NewConsumer(pool *ConnectionPool, queue string) (<-chan amqp.Delivery, error) {
-	consumer := &Consumer{Pool: pool}
-	err := consumer.declareQueue(queue)
+func NewConsumer(pool *ConnectionPool, queueName, exchangeName, routKey string, durable bool, args amqp.Table) (<-chan amqp.Delivery, error) {
+	consumer := &Consumer{pool: pool, queueName: queueName, durable: durable, exchangeName: exchangeName, routKey: routKey}
+	return consumer.declareQueueAndGetNewDeliveryChannel(args)
+}
+
+func (c *Consumer) declareQueueAndGetNewDeliveryChannel(args amqp.Table) (<-chan amqp.Delivery, error) {
+	ch := c.pool.Get()
+	queue, err := ch.QueueDeclare(c.queueName, c.durable, false, false, false, nil)
 	if err != nil {
 		return nil, err
 	}
-
-	return consumer.newDeliveryChannel()
-}
-
-func (c *Consumer) declareQueue(q string) error {
-	if q == "" {
-		return errors.New("no queue name has been specified for consumer")
+	err = ch.QueueBind(queue.Name, c.routKey, c.exchangeName, false, nil)
+	if err != nil {
+		return nil, err
 	}
-	c.Queue = q
-	return nil
-}
-
-func (c *Consumer) newDeliveryChannel() (<-chan amqp.Delivery, error) {
-	ch := c.Pool.Get()
-	deliveryChannel, err := ch.Consume(c.Queue, "", false, false, false, false, nil)
+	deliveryChannel, err := ch.Consume(queue.Name, "", false, false, false, false, args)
 
 	if err != nil {
 		return nil, err
 	}
 
 	return deliveryChannel, nil
-
 }
